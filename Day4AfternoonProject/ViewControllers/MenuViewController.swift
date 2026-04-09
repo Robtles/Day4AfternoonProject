3558fe276b0baf7420586cecc212065ef9cab44f
Robtles <robbeyroad@gmail.com>
2026-04-09 16:28:56 +0200

    Contacts feature skeleton

--- Day4AfternoonProject/ViewControllers/MenuViewController.swift
+++ Day4AfternoonProject/ViewControllers/MenuViewController.swift
@@ -7,18 +7,21 @@ import UIKit
 
 enum MenuRoute: CaseIterable {
     case camera
+    case contacts
 
     var title: String {
         switch self {
-        case .camera:
-            return "TO CAMERA FEATURE"
+        case .camera: "TO CAMERA FEATURE"
+        case .contacts: "TO CONTACTS FEATURE"
         }
     }
 
     var viewController: UIViewController {
         switch self {
         case .camera:
-            return CameraViewController()
+            CameraViewController()
+        case .contacts:
+            ContactsViewController()
         }
     }
 }
